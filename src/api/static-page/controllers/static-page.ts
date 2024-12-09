8eec327277b76317a2088c128a47105b90926b84
Saad Javaid Alvi <saadjavaidalvi@gmail.com>
2024-12-09 18:57:07 +0500

    added models for static pages content types

---
+++ src/api/static-page/controllers/static-page.ts
@@ -0,0 +1,8 @@
+/**
+ * static-page controller
+ */
+
+import {factories} from '@strapi/strapi'
+
+export default factories.createCoreController('api::static-page.static-page');
+
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,60 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface StaticPagesContentSection extends Struct.ComponentSchema {
+  collectionName: 'components_static_pages_content_sections';
+  info: {
+    displayName: 'ContentSection';
+  };
+  attributes: {
+    primaryDescription: Schema.Attribute.Text;
+    secondaryDescription: Schema.Attribute.Text;
+  };
+}
+
+export interface StaticPagesFeaturedGridBlock extends Struct.ComponentSchema {
+  collectionName: 'components_static_pages_featured_grid_block';
+  info: {
+    description: '';
+    displayName: 'FeaturedGridBlock';
+  };
+  attributes: {
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface StaticPagesHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_static_pages_hero_section';
+  info: {
+    description: '';
+    displayName: 'HeroSection';
+  };
+  attributes: {
+    images: Schema.Attribute.Media<'images', true> & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface StaticPagesImageGallery extends Struct.ComponentSchema {
+  collectionName: 'components_static_pages_image_gallery';
+  info: {
+    description: '';
+    displayName: 'ImageGallery';
+  };
+  attributes: {
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
+    images: Schema.Attribute.Media<'images', true> & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'static-pages.content-section': StaticPagesContentSection;
+      'static-pages.featured-grid-block': StaticPagesFeaturedGridBlock;
+      'static-pages.hero-section': StaticPagesHeroSection;
+      'static-pages.image-gallery': StaticPagesImageGallery;
+    }
+  }
+}

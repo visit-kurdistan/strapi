8c41def0a357ab547f5a0c465c5024a8b815d473
Ibtihaaj <ibtihaaj@we-over-i.com>
2025-04-30 02:26:10 +0500

    chore: ability to fetch 5 level nested data

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,8 +3,15 @@ export default () => ({
     config: {
       provider: "local",
       providerOptions: {
-        sizeLimit: 500 * 1024 * 1024, // 500MB
+        sizeLimit: 5 * 1024 * 1024, // 5MB
       },
+    },
+  },
+
+  // nested populator
+  "strapi-plugin-nested-populator": {
+    config: {
+      defaultDepth: 5, // 5 levels of depth
     },
   },
 });
